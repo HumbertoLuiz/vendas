99b24ec023dbf1fb459621c94119f95f688f7a27
HumbertoLuiz <kaos_132@hotmail.com>
2022-05-30 21:27:38 -0300

    @Get, @Post, @Put, @Delete

--- src/main/java/io/github/humbertoluiz/api/controller/ClienteController.java
+++ src/main/java/io/github/humbertoluiz/api/controller/ClienteController.java
@@ -1,12 +1,70 @@
 package io.github.humbertoluiz.api.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import io.github.humbertoluiz.domain.entity.Cliente;
+import io.github.humbertoluiz.domain.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/v1/clientes")
 public class ClienteController {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
 
+	@GetMapping("/{clienteId}")
+	public Cliente getClienteById(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+			.orElseThrow(() ->
+			new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente save(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+
+	@DeleteMapping("/{clienteId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Cliente delete(@PathVariable Long clienteId) {
+		return clienteRepository
+		.findById(clienteId)
+		.map(cliente -> clienteRepository.save(cliente))
+		.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@PutMapping("/{clienteId}")
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId)
+			.map(clienteExistente -> {
+			cliente.setId(clienteExistente.getId());
+			clienteRepository.save(cliente);
+			return clienteExistente;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@GetMapping
+	public List<Cliente> find(Cliente filtro) {
+		ExampleMatcher matcher = ExampleMatcher
+				.matching()
+				.withIgnoreCase()
+				.withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+		Example<Cliente> example = Example.of(filtro, matcher);
+		return clienteRepository.findAll(example);
+	}
 
 }
